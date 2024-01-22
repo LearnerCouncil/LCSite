0259baefd1bf8ed2c4044789188624e52ee64612
NullCube <nullcub3@gmail.com>
2024-01-21 20:24:52 -0800

    Copy LC-Testing-Site

---
+++ src/discord-server.md
@@ -0,0 +1,10 @@
+---
+title: Discord Server
+layout: "base.njk"
+---
+
+# {{ title }}
+
+Want to join our wonderful discord server?
+
+Talk to ALP. (no, really)
---
+++ src/scripts/color-mode.js
@@ -0,0 +1,36 @@
+var colorMode = window.localStorage.getItem("color-mode");
+var colorModeButton = document.getElementById("color-mode-button"); 
+
+if(colorMode === null) {
+	if(window.matchMedia("(prefers-color-scheme: light)").matches) {
+		colorMode = "light";
+	} else {
+		colorMode = "dark";
+	}
+}
+update();
+
+colorModeButton.addEventListener("click", toggle);
+
+function update() {
+	if(colorMode === "light"){
+		//colorModeButton.firstElementChild.src = "images/moon.svg";
+		colorModeButton.style.backgroundImage = "url(../images/moon.svg)";
+		document.documentElement.setAttribute("color-mode", "light");
+		window.localStorage.setItem("color-mode", "light");
+	} else {
+		//colorModeButton.firstElementChild.src = "images/sun.svg";
+		colorModeButton.style.backgroundImage = "url(../images/sun.svg)";
+		document.documentElement.setAttribute("color-mode", "dark");
+		window.localStorage.setItem("color-mode", "dark");
+	}
+}
+
+function toggle() {
+	if(colorMode === "light") {
+		colorMode = "dark";
+	} else {
+		colorMode = "light";
+	}
+	update();
+}
